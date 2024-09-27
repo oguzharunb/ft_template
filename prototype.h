f4c1f2f188fd9b213b234e3e4589c2ebc461531c
oguzharunb <oguzharundevp@protonmail.com>
2024-09-27 11:35:52 +0300

    prototype error

--- prototype.h
+++ prototype.h
@@ -2,6 +2,6 @@
 # define PROTOTYPE_H // change to your header
 # include <stddef.h> // add libraries as you like
 
-//prototypes -> dont touch here, script will automatically fill this place
+//prototypes
 
 #endif
